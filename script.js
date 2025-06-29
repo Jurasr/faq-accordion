fda8cd6ed6c8b054b2c8738c53527d02e240b8a6
Juras <jrabacauskas@gmail.com>
2025-06-29 09:39:00 +0300

    Added on open faq closes all other

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const toggles = document.querySelectorAll(".toggle");
+let activeToggle = toggles[0];
 
 toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
-    const active = Array.from(toggle.classList).includes("active");
-    const answer = toggle.parentNode.parentNode.querySelector(".answer");
-    console.log(active, answer);
+    const faq = toggle.parentElement.parentElement;
+    const active = Array.from(faq.classList).includes("active");
     if (active) {
+      faq.classList.remove("active");
       toggle.src = "./assets/images/icon-plus.svg";
-      toggle.classList.remove("active");
-      answer.classList.remove("active");
     } else {
+      faq.classList.add("active");
       toggle.src = "./assets/images/icon-minus.svg";
-      toggle.classList.add("active");
-      answer.classList.add("active");
+      if (toggle !== activeToggle) {
+        activeToggle.parentElement.parentElement.classList.remove("active");
+        activeToggle.src = "./assets/images/icon-plus.svg";
+        activeToggle = toggle;
+      }
     }
   });
 });
